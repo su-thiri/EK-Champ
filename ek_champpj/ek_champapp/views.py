145c9ca36e9a401fc863324473214ac9ff5d3473
waiwai707 <waiwai07077@gmail.com>
2024-12-11 17:35:46 +0700

    `Added views for Round, Team_numbers, and Team_in_round models with CRUD operations`

--- ek_champpj/ek_champapp/views.py
+++ ek_champpj/ek_champapp/views.py
@@ -1,15 +1,14 @@
-from django.shortcuts import render,redirect
-from ek_champapp.models  import Round, Team_numbers, Team_in_round
+from django.shortcuts import render, redirect
+from ek_champapp.models import Round, Team_numbers, Team_in_round
+
 # Create your views here.
 
-
-
 def base_round(request):
-    data = Round.objects.raw('select * from ')
-    return render(request,'pages/round.html',{'data':data})
+    data = Round.objects.raw('SELECT * FROM ek_champapp_round')  
+    return render(request, 'round.html', {'data': data})
 
 def create_round(request):
-    return render(request,'/create_round.html')
+    return render(request, 'create_round.html')
 
 def store_round(request):
     rdata = Round()
@@ -23,16 +22,16 @@ def store_round(request):
     rdata.base_weight = request.POST.get('base_weight')
     rdata.max_weight = request.POST.get('max_weight')
     rdata.race_duration_time = request.POST.get('race_duration_time')
-    rdata.pitlane = request.POST.get('pitlan')
-    rdata.save
+    rdata.pitlane = request.POST.get('pitlane')
+    rdata.save()
     return redirect('/base_round')
 
-def edit_round(request,pk):
-    update_r = Round.objects.get(id = pk)
-    return render(request,'edit_round.html',{'update_r':update_r})
+def edit_round(request, pk):
+    update_r = Round.objects.get(id=pk)
+    return render(request, 'edit_round.html', {'update_r': update_r})
 
-def editdata_round(request,pk):
-    edata = Round.objects.get(id = pk)
+def editdata_round(request, pk):
+    edata = Round.objects.get(id=pk)
     edata.round_id = request.POST.get('id')
     edata.championship_name = request.POST.get('championship_name')
     edata.round_name = request.POST.get('round_name')
@@ -43,16 +42,21 @@ def editdata_round(request,pk):
     edata.base_weight = request.POST.get('base_weight')
     edata.max_weight = request.POST.get('max_weight')
     edata.race_duration_time = request.POST.get('race_duration_time')
-    edata.pitlane = request.POST.get('pitlan')
+    edata.pitlane = request.POST.get('pitlane')
     edata.save()
     return redirect('/base_round')
 
+def delete_round(request, pk):
+    ddata = Round.objects.get(id=pk) 
+    ddata.delete() 
+    return redirect('/base_round')
+
 def base_teamno(request):
-    data = Team_numbers.objects.raw('select * from ')
-    return render(request,'teamno.html',{'data':data})
+    data = Team_numbers.objects.raw('SELECT * FROM ek_champapp_team_numbers')
+    return render(request, 'teamno.html', {'data': data})
 
 def create_teamno(request):
-    return render(request,'create_teammo.html')
+    return render(request, 'create_teamno.html')
 
 def store_teamno(request):
     tdata = Team_numbers()
@@ -63,12 +67,12 @@ def store_teamno(request):
     tdata.save()
     return redirect('/base_teamno')
 
-def edit_teamno(request,pk):
-    update_teamno = Team_numbers.objects.get(id = pk)
-    return render(request,'edit_teamno.html',{'update_teamno',update_teamno})
+def edit_teamno(request, pk):
+    update_teamno = Team_numbers.objects.get(id=pk)
+    return render(request, 'edit_teamno.html', {'update_teamno': update_teamno})
 
-def editdata_teamno(request,pk):
-    edata = Team_numbers.objects.get(id = pk)
+def editdata_teamno(request, pk):
+    edata = Team_numbers.objects.get(id=pk)
     edata.team_number_id = request.POST.get('team_number_id')
     edata.championship_id = request.POST.get('championship_id')
     edata.team_id = request.POST.get('team_id')
@@ -76,12 +80,17 @@ def editdata_teamno(request,pk):
     edata.save()
     return redirect('/base_teamno')
 
-def base_tinround(request):
-    data = Team_in_round.objects.raw('select * from ')
-    return render(request,'tinround.html',{'data':data})
+def delete_teamno(request, pk): 
+    ddata = Team_numbers.objects.get(id=pk)
+    ddata.delete() 
+    return redirect('/base_teamno')
 
-def crete_tinround(request):
-    return render(request,'create_tinround.html')
+def base_tinround(request):
+    data = Team_in_round.objects.raw('SELECT * FROM ek_champapp_team_in_round')
+    return render(request, 'tinround.html', {'data': data})
+
+def create_tinround(request):
+    return render(request, 'create_tinround.html')
 
 def store_tinround(request):
     tinrdata = Team_in_round()
@@ -98,12 +107,12 @@ def store_tinround(request):
     tinrdata.save()
     return redirect('/base_tinround')
 
-def edit_tinround(request,pk):
-    update_tinr = Team_in_round.objects.get(id = pk)
-    return render(request,'edit_tinround.html',{'update_tinr':update_tinr})
+def edit_tinround(request, pk):
+    update_tinr = Team_in_round.objects.get(id=pk)
+    return render(request, 'edit_tinround.html', {'update_tinr': update_tinr})
 
-def edata_tinround(request,pk):
-    edata = Team_in_round.objects.get(id = pk)
+def editdata_tinround(request, pk):
+    edata = Team_in_round.objects.get(id=pk)
     edata.id = request.POST.get('id')
     edata.championship = request.POST.get('championship')
     edata.round = request.POST.get('round')
@@ -117,3 +126,7 @@ def edata_tinround(request,pk):
     edata.save()
     return redirect('/base_tinround')
 
+def delete_tinround(request, pk): 
+    ddata = Team_in_round.objects.get(id=pk) 
+    ddata.delete() 
+    return redirect('/base_tinround')
